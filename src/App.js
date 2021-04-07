066f3dfb47693c01382dedb55c22a885a024beb3
LucioApc <67089165+LucioApc@users.noreply.github.com>
2021-04-06 22:46:43 -0500

    ADD React-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+
 import Navigation from './components/Navigation';
+import NotesList from './components/NotesList';
+import CreateNote from './components/CreateNote';
+import CreateUser from './components/CreateUser';
 
 function App() {
 	return (
-		<div>
-			<Navigation></Navigation>
-			Hello World
-		</div>
+		<Router>
+			<Navigation />
+			<Route path="/" exact component={NotesList} />
+			<Route path="/edit/:id" component={CreateNote} />
+			<Route path="/create" component={CreateNote} />
+			<Route path="/user" component={CreateUser} />
+		</Router>
 	);
 }
 
